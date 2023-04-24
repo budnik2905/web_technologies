9bb1c1323695e91088538f5dedac2d0f0aa39bcd
budnik2905 <budnik2905@gmail.com>
2023-04-24 23:22:03 +0300

    семинар 3

---
+++ homework3/1.js
@@ -0,0 +1,5 @@
+let temperatureCelsius = prompt("Введите температуру в градусах Цельсия:");
+
+let temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
+
+alert(`Цельсий: ${temperatureCelsius}, Фаренгейт: ${temperatureFahrenheit}`);
---
+++ homework3/2.js
@@ -0,0 +1,18 @@
+function greeting(name) {
+    console.log(`Привет, ${name}!`);
+  }
+  
+  const userName = prompt("Введите ваше имя:");
+  greeting(userName)
+  
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
