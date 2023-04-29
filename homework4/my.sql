5a87c1aaca8919ce00b33b8b58c0fadc8a4250f5
budnik2905 <budnik2905@gmail.com>
2023-04-29 13:07:00 +0300

    семинар 4

---
+++ homework4/my.sql
@@ -0,0 +1,18 @@
+-- create
+CREATE TABLE EMPLOYEE (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age TEXT NOT NULL,
+    address TEXT NOT NULL
+);
+-- insert
+INSERT INTO EMPLOYEE
+VALUES (0001, 'Белогор', '18', 'Москва');
+INSERT INTO EMPLOYEE
+VALUES (0002, 'Ведогор', '92', 'Каломна');
+INSERT INTO EMPLOYEE
+VALUES (0003, 'Горисвет', '95', 'Муром');
+INSERT INTO EMPLOYEE
+VALUES (0004, 'Доброслав', '18', 'Москва');
+INSERT INTO EMPLOYEE
+VALUES (0005, 'Звенимир', '36', 'Москва');
